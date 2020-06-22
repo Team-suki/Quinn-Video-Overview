857257cb7aafebc9016bfe43ea1a4d2c388a841a
Vasan Arul <vasankarul@gmail.com>
2020-06-22 11:51:17 -0700

    Committed code should reflect Video and Banner model creation

--- database/index.js
+++ database/index.js
@@ -14,6 +14,29 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
+class Video extends Model {}
+Video.init ({
+  title: Sequelize.STRING,
+  description: Sequelize.STRING,
+  video_url: Sequelize.STRING,
+  bannerID: Sequelize.INTEGER
+}, {sequelize, modelName: 'video'});
+
+class Banner extends Model {}
+Banner.init ({
+  title: Sequelize.STRING,
+  description: Sequelize.STRING,
+  amount_pledged: Sequelize.INTEGER,
+  goal: Sequelize.STRING,
+  backers: Sequelize.INTEGER,
+  backers_text: Sequelize.STRING,
+  days: Sequelize.INTEGER,
+  days_text: Sequelize.STRING,
+  all_or_nothing: Sequelize.BOOLEAN,
+  location: Sequelize. STRING,
+  project_we_love: Sequelize.BOOLEAN
+}, {sequelize, modelName: 'banner'});
+
 sequelize.sync();
 
 
