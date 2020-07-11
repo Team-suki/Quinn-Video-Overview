e75212bedda7fb1f66fe717c323799d6f5816ed3
Quinn Royston <quinn.royston@gmail.com>
2020-07-11 13:41:29 -0700

    removed unnecessary files

--- database/seedPostgres.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-var read = fs.createReadStream('./finaldata.json', { start: 0, end: 99 }).on('open', ()=>{
-})
-
-//read.read(0).then((data)=> console.log(data))
-
-read.close()
-
