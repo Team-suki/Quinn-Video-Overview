a2c10becf1725b3b6abf9d289bec0e13c8fe5b4b
Quinn Royston <quinn.royston@gmail.com>
2020-07-11 15:53:52 -0700

    update banner and video components to use new endpoints

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -19,10 +19,11 @@ class Banner extends React.Component {
   getBanner() {
     //console.log(window.location.pathname);
     axios.get(`http://localhost:3002/banners${window.location.pathname}`).then(result => {
-      //console.log(result.data);
+      console.log(result.data);
       this.setState({
         title: result.data.title,
-        description: result.data.description
+        description: result.data.description,
+        location: result.data.location,
       });
     })
   }
