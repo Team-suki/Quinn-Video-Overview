1481cff5470f8ed377319618f2423a0ed42482cc
Quinn Royston <quinn.royston@gmail.com>
2020-07-18 11:08:25 -0700

    Added error handling for root path

--- server/index.js
+++ server/index.js
@@ -18,7 +18,11 @@ app.use(express.static(path.join(__dirname, '../dist')));
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  try {
+    res.sendFile(path.join(__dirname, '../dist', 'index.html'));
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 
