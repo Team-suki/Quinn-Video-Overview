3426f6566082de9e2e910c09867bd2735fca4e8c
Quinn Royston <quinn.royston@gmail.com>
2020-07-21 20:19:19 -0700

    transitioned to hydrate to prep for SSR

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('banner'));
+ReactDOM.hydrate(<App />, document.getElementById('banner'));
