fc90e2cec17b4236ef6b22fc96b67a2fec97753d
Quinn Royston <quinn.royston@gmail.com>
2020-07-22 23:37:36 -0700

    setup local machine to work with redis

--- database/redis.js
+++ database/redis.js
@@ -1,6 +1,6 @@
 const redis = require('redis');
 
-const connection = () => {return redis.createClient({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT})};
+const connection = () => {return redis.createClient({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, no_ready_check: true, password: process.env.REDIS_PASSWORD})};
 
 module.exports.connection = connection;
 module.exports.redis = redis;
