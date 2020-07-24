984f93f7361e83ecc7043210059d2d8789ac7ba8
Quinn Royston <quinn.royston@gmail.com>
2020-07-23 20:22:57 -0700

    set redis to log errors

--- database/redis.js
+++ database/redis.js
@@ -2,5 +2,9 @@ const redis = require('redis');
 
 const redisClient = redis.createClient({host: process.env.REDIS_HOST, port: process.env.REDIS_PORT, no_ready_check: true, password: process.env.REDIS_PASSWORD});
 
+redisClient.on('error', function (err) {
+  console.log('Something went wrong ' + err);
+});
+
 module.exports.redisClient = redisClient;
 module.exports.redis = redis;
